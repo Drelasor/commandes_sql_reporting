55b1981a0f795e3502694ff9dffbba8dc6f2d4b5
Raphaël Grand <otumbarg@gmail.com>
2023-08-31 11:53:17 +0200

    2 requetes es specifiques

--- es_specifique.sql
+++ es_specifique.sql
@@ -1,14 +1,21 @@
 select distinct
+spic.SP_VALUE, spic.IC_SHORT_DESC,spii.DSP_TITLE ,spii.IIVALUE
 
-sp.SP, sp.SP_VALUE, sp.SP_VERSION, spic.IC_SHORT_DESC, spii.DSP_TITLE, spsp.SP as SPSP_SP, spsp.CHILD_SP, spchild.SP as SPCHILD,  spchild.SP_VALUE as SPCHILD_VALUE, spchild.SP_VERSION as SPCHILD, spicchild.IC_SHORT_DESC, spiichild.DSP_TITLE
+from  RndSuite.RndtSpSp as spsp
 
-from RndSuite.RndtSp as sp
-left join RndSuite.RndtSpIc as spic on sp.SP = spic.SP and sp.SP_VERSION = spic.SP_VERSION
+left join RndSuite.RndtSpIc as spic on spic.SP = spsp.CHILD_SP 
 left join RndSuite.RndtSpIi as spii on spii.IC = spic.IC
-left join RndSuite.RndtSpSp as spsp on spsp.SP = sp.SP
-left join RndSuite.RndtSp as spchild on spchild.SP = spsp.CHILD_SP
-left join RndSuite.RndtSpIc as spicchild on spchild.SP = spicchild.SP and spchild.SP_VERSION = spicchild.SP_VERSION
-left join RndSuite.RndtSpIi as spiichild on spiichild.IC = spicchild.IC
 
+where spsp.CHILD_SP = 106
 
-where sp.SP = 110 and spchild.SP = 106
+-----
+
+select distinct
+spic.SP_VALUE, spic.IC_SHORT_DESC,spii.DSP_TITLE ,spii.IIVALUE
+
+from  RndSuite.RndtSpSp as spsp
+
+left join RndSuite.RndtSpIc as spic on spic.SP = spsp.SP
+left join RndSuite.RndtSpIi as spii on spii.IC = spic.IC
+
+where spsp.CHILD_SP = 106
