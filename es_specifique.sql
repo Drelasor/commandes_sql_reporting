94f0b3718a8d2a0f5a4e1fb890915e3d67d970a1
Raphaël Grand <otumbarg@gmail.com>
2023-09-05 17:56:52 +0200

    reglages bugs

--- es_specifique.sql
+++ es_specifique.sql
@@ -1,21 +1,28 @@
+// spécifique
 select distinct
-spic.SP_VALUE, spic.IC_SHORT_DESC,spii.DSP_TITLE ,spii.IIVALUE
+spicchild.SP_VALUE, spicchild.IC_SHORT_DESC,spiichild.DSP_TITLE ,spiichild.IIVALUE
+
 
 from  RndSuite.RndtSpSp as spsp
+ join RndSuite.RndtSpIc as spicchild on spicchild.SP = spsp.CHILD_SP
+ join RndSuite.RndtSpIc as spic on spic.SP = spsp.SP
+ join RndSuite.RndtSpIi as spiichild on spiichild.IC = spicchild.IC and spiichild.SP = spicchild.SP
 
-left join RndSuite.RndtSpIc as spic on spic.SP = spsp.CHILD_SP 
-left join RndSuite.RndtSpIi as spii on spii.IC = spic.IC and spii.SP = spic.SP
 
-where spsp.CHILD_SP = 106
 
+where spicchild.SP_VALUE = 'SP20230522-3'
 -----
 
+// générique
 select distinct
-spic.SP_VALUE, spic.IC_SHORT_DESC,spii.DSP_TITLE ,spii.IIVALUE
+spic.SP_VALUE, spic.IC_SHORT_DESC,spii.DSP_TITLE,spii.IIVALUE
+
 
 from  RndSuite.RndtSpSp as spsp
+ join RndSuite.RndtSpIc as spicchild on spicchild.SP = spsp.CHILD_SP
+ join RndSuite.RndtSpIc as spic on spic.SP = spsp.SP
+ join RndSuite.RndtSpIi as spii on spii.IC = spic.IC and spii.SP = spic.SP
 
-left join RndSuite.RndtSpIc as spic on spic.SP = spsp.SP
-left join RndSuite.RndtSpIi as spii on spii.IC = spic.IC and spii.SP = spic.SP
 
-where spsp.CHILD_SP = 106
+
+where spicchild.SP_VALUE = 'SP20230522-3'
