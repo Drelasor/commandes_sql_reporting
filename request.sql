1711d41675c16f3aedf5f8484773be70295d6237
Raphaël Grand <otumbarg@gmail.com>
2024-04-24 17:43:20 +0200

    implementation du ppsg (en cours)

--- request.sql
+++ request.sql
@@ -1,39 +1,22 @@
-//Contexte Eurogerm
+select distinct
 
-select distinct 
-rqP.RQ_VALUE as DEC,rqPLang.DESCRIPTION, 
-rqC.RQ_VALUE as Formulation, rqC.DESCRIPTION,
-sp.SP_VALUE as client,
-splang.DESCRIPTION, 
-rqCic.DESCRIPTION,
-rqCii.DSP_TITLE as Form_DSP_TITLE,
-rqCii.IIVALUE as Form_IIVALUE
+sp.SP, spii.II_SHORT_DESC, spii.DSP_TITLE, spii.IIVALUE, ppsgRow.DESCRIPTION
 
+from RndSuite.RndtRq as form
 
---Selection du DEC
-from RndSuite.RndtRq as rqP 
-join RndSuite.RndtRqLang as rqPLang on rqPLang.RQ = rqP.RQ
-join RndSuite.RndtRqIc as rqPic on rqPic.RQ = rqP.RQ 
-join RndSuite.RndtRqIi as rqPii on rqPii.RQ = rqPic.RQ and rqPii.IC = rqPic.IC and rqPii.ICNODE = rqPic.ICNODE
+join RndSuite.RndtRqIc as formIc on formIc.RQ = form.RQ
+join RndSuite.RndtRqIcLang as formIcLang on formIcLang.RQ = formIc.RQ and formIcLang.IC = formIc.IC and formIcLang.ICNODE = formIc.ICNODE  
 
+join RndSuite.RndtRqIi as formIi on formIi.RQ = formIc.RQ and formIi.IC = formIc.IC and formIi.ICNODE = formIc.ICNODE
+join RndSuite.RndtRqIiLang as formIiLang on formIiLang.RQ = formIi.RQ and formIiLang.II = formIi.II and formIiLang.IINODE = formIi.IINODE  and formIiLang.IC = formIi.IC and formIiLang.ICNODE = formIi.ICNODE
 
-
---Selection de la formulation du DEC 
-join RndSuite.RndtRqRq as rqrq on rqrq.RQ = rqP.RQ 
-join RndSuite.RndtRq as rqC on  rqrq.RQ_CHILD = rqC.RQ
-join RndSuite.RndtRqIc as rqCic on rqCic.RQ = rqC.RQ 
-join RndSuite.RndtRqIi as rqCii on rqCii.RQ = rqCic.RQ and rqCii.IC = rqCic.IC and rqCii.ICNODE = rqCic.ICNODE
+join RndSuite.RndtFmMat as mat on form.RQ = mat.RQ
+join RndSuite.RndtSp as sp on sp.SP = mat.SP and sp.SP_VERSION = mat.SP_VERSION
+join RndSuite.RndtSpIi as spii on spii.SP = sp.SP and spii.SP_VERSION = sp.SP_VERSION
+join RndSuite.RndtSpIiPpsgCell as ppsgCell on ppsgCell.SP = sp.SP and ppsgCell.SP_VERSION = sp.SP_VERSION and ppsgCell.IINODE = spii.IINODE
+join RndSuite.RndtSpIiPpsgRow as ppsgRow on ppsgRow.SP = sp.SP and ppsgRow.SP_VERSION = sp.SP_VERSION and ppsgRow.IINODE = spii.IINODE
 
 
---Selection du client lié au DEC
-join RndSuite.RndtRqSp as rqsp on rqP.RQ = rqsp.RQ
-join RndSuite.RndtSp as sp on sp.SP = rqsp.SP and sp.SP_VERSION in (
-    select CASE rqsp.SP_VERSION when -1 then 
-        (select TOP 1 first_value(x.SP_VERSION)
-        over(order by x.ACTIVE desc, x.SP_VERSION desc) from RndSuite.RndtSp x where x.SP = sp.SP)
-else rqsp.SP_VERSION end) 
-join RndSuite.RndtSpLang as splang on splang.SP = sp.SP and splang.SP_VERSION = sp.SP_VERSION
-join RndSuite.RndtSpIc as spic  on spic.SP=sp.SP and spic.SP_VERSION = sp.SP_VERSION
-join RndSuite.RndtSpIi as spii on spii.SP = spic.SP and spii.IC = spic.IC and spii.ICNODE = spic.ICNODE
 
-where rqP.RQ = 199
+
+where form.RQ = 230 and spii.II_SHORT_DESC like 'MDD_assigned_PF'
