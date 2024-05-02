f49372a0c2ccda24ba089f117ade8f3d62ead3c7
Raphaël Grand <otumbarg@gmail.com>
2024-05-02 18:03:24 +0200

    gestion ppsg

--- request.sql
+++ request.sql
@@ -20,59 +20,7 @@ and spii.II_SHORT_DESC like 'MDD_assigned_PF' ;
 
 
 
-// récuperation ligne de production 
-
-
-select distinct
-
-sp.SP, ppsg_row.DESCRIPTION
-
-from RndSuite.RndtRq as form
-
-join RndSuite.RndtRqIc as formIc on formIc.RQ = form.RQ
-join RndSuite.RndtRqIcLang as formIcLang on formIcLang.RQ = formIc.RQ and formIcLang.IC = formIc.IC and formIcLang.ICNODE = formIc.ICNODE  
-
-join RndSuite.RndtRqIi as formIi on formIi.RQ = formIc.RQ and formIi.IC = formIc.IC and formIi.ICNODE = formIc.ICNODE
-join RndSuite.RndtRqIiLang as formIiLang on formIiLang.RQ = formIi.RQ and formIiLang.II = formIi.II and formIiLang.IINODE = formIi.IINODE  and formIiLang.IC = formIi.IC and formIiLang.ICNODE = formIi.ICNODE
-
-join RndSuite.RndtFmMat as mat on form.RQ = mat.RQ 
-join RndSuite.RndtSp as sp on sp.SP = mat.SP and sp.SP_VERSION = mat.SP_VERSION
-join RndSuite.RndtSpIc as spic on spic.SP = sp.SP and spic.SP_VERSION = sp.SP_VERSION
-join RndSuite.RndtSpIi as spii on spii.SP = spic.SP and spii.SP_VERSION = spic.SP_VERSION and spii.ICNODE = spic.ICNODE and spii.IC = spic.IC	
-join RndSuite.RndtSpIiPpsgRow as ppsg_row on ppsg_row.SP = spii.SP and ppsg_row.SP_VERSION = spii.SP_VERSION and ppsg_row.IC = spii.IC and ppsg_row.ICNODE = spii.ICNODE and ppsg_row.IINODE = spii.IINODE
-
-	
-where form.RQ_VALUE = 'FM20240423-6' and spii.IDENTIFIER like 'Mixers'
-
-
-// occurences production
-
-select valeur_distincte , COUNT(*) AS occurences_ligne
-FROM (
-    SELECT DISTINCT ppsg_row.DESCRIPTION AS valeur_distincte, sp.SP as valeur_sp
-	from RndSuite.RndtRq as form
-
-join RndSuite.RndtRqIc as formIc on formIc.RQ = form.RQ
-join RndSuite.RndtRqIcLang as formIcLang on formIcLang.RQ = formIc.RQ and formIcLang.IC = formIc.IC and formIcLang.ICNODE = formIc.ICNODE  
-
-join RndSuite.RndtRqIi as formIi on formIi.RQ = formIc.RQ and formIi.IC = formIc.IC and formIi.ICNODE = formIc.ICNODE
-join RndSuite.RndtRqIiLang as formIiLang on formIiLang.RQ = formIi.RQ and formIiLang.II = formIi.II and formIiLang.IINODE = formIi.IINODE  and formIiLang.IC = formIi.IC and formIiLang.ICNODE = formIi.ICNODE
-
-join RndSuite.RndtFmMat as mat on form.RQ = mat.RQ 
-join RndSuite.RndtSp as sp on sp.SP = mat.SP and sp.SP_VERSION = mat.SP_VERSION
-join RndSuite.RndtSpIc as spic on spic.SP = sp.SP and spic.SP_VERSION = sp.SP_VERSION
-join RndSuite.RndtSpIi as spii on spii.SP = spic.SP and spii.SP_VERSION = spic.SP_VERSION and spii.ICNODE = spic.ICNODE and spii.IC = spic.IC	
-join RndSuite.RndtSpIiPpsgRow as ppsg_row on ppsg_row.SP = spii.SP and ppsg_row.SP_VERSION = spii.SP_VERSION and ppsg_row.IC = spii.IC and ppsg_row.ICNODE = spii.ICNODE and ppsg_row.IINODE = spii.IINODE
-
-where form.RQ_VALUE = 'FM20240423-6' and spii.IDENTIFIER like 'Mixers'
-
- ) AS sous_requete
-
- GROUP BY valeur_distincte ;
-
-
-
-// fusion 
+// fusion occurence production + nombre de sp contenant une ligne de production 
 
 select valeur_distincte , COUNT(*) AS occurences_ligne, nombre_sp
 FROM (
@@ -122,3 +70,42 @@ CROSS JOIN (
 ) AS test
 
 group by valeur_distincte,  nombre_sp
+
+// ppsg 
+
+select distinct
+
+sp.SP_VALUE, spic.DSP_TITLE,ppsg_row.DESCRIPTION, cell.VALUE, cell.VALUE_S, cell.PR_SEQ, ppsg_row.PR_SEQ,cell.COL_TP
+
+from RndSuite.RndtRq as form
+
+join RndSuite.RndtRqIc as formIc on formIc.RQ = form.RQ
+join RndSuite.RndtRqIcLang as formIcLang on formIcLang.RQ = formIc.RQ and formIcLang.IC = formIc.IC and formIcLang.ICNODE = formIc.ICNODE  
+
+join RndSuite.RndtRqIi as formIi on formIi.RQ = formIc.RQ and formIi.IC = formIc.IC and formIi.ICNODE = formIc.ICNODE
+join RndSuite.RndtRqIiLang as formIiLang on formIiLang.RQ = formIi.RQ and formIiLang.II = formIi.II and formIiLang.IINODE = formIi.IINODE  and formIiLang.IC = formIi.IC and formIiLang.ICNODE = formIi.ICNODE
+
+join RndSuite.RndtFmMat as mat on form.RQ = mat.RQ 
+join RndSuite.RndtSp as sp on sp.SP = mat.SP and sp.ACTIVE = 1
+join RndSuite.RndtSpIc as spic on spic.SP = sp.SP 
+join RndSuite.RndtSpIi as spii on spii.SP = spic.SP and spii.SP_VERSION = spic.SP_VERSION and spii.ICNODE = spic.ICNODE and spii.IC = spic.IC	
+join RndSuite.RndtSpIiPpsgRow as ppsg_row on ppsg_row.SP = spii.SP and ppsg_row.SP_VERSION = spii.SP_VERSION and ppsg_row.IC = spii.IC and ppsg_row.ICNODE = spii.ICNODE and ppsg_row.IINODE = spii.IINODE
+join RndSuite.RndtSpIiPpsgCell cell on ppsg_row.SP = cell.SP and ppsg_row.SP_VERSION = ppsg_row.SP_VERSION and ppsg_row.IC = cell.IC and ppsg_row.ICNODE = cell.ICNODE and ppsg_row.IINODE = cell.IINODE and cell.PR_SEQ = ppsg_row.PR_SEQ
+--join RndSuite.RndvRepSpIiComp comp on spii.SP = comp.SP and spii.SP_VERSION = comp.SP_VERSION and spii.IC = comp.IC and spii.ICNODE = comp.ICNODE and spii.IINODE = comp.IINODE
+--join RndSuite.RndtSpIiLy spiily on spii.SP = spiily.SP and spii.SP_VERSION = spiily.SP_VERSION and spii.ICNODE = spiily.ICNODE and spii.IINODE = spiily.IINODE
+--join (select LY, VERSION as LY_VERSION, SEQ as LY_SEQ, COL_ID, 
+--        ltrim(rtrim(substring(COL_TP,1,charindex('@',COL_TP,1)-1))) as COL_TYPE,
+--              ltrim(rtrim(substring(COL_TP,charindex('@',COL_TP,1)+1,1000))) as COL_LINK,
+--              DISP_TITLE,
+--              DISP_WIDTH,
+--              COL_HIDDEN,
+--              COL_LEN,
+--              COL_ALIGNMENT,
+--              COL_ALIGNMENT_HEADER,
+--              COUNT(SEQ) over (partition by LY, VERSION) as COL_NUMBER
+--   from RndSuite.RndtLyDetails
+--  where COL_HIDDEN <> 1) e on spiily.LY = e.LY and spiily.LY_VERSION = e.LY_VERSION
+
+
+where form.RQ_VALUE = 'FM20240416-2' and spic.IC = 146  and sp.SP_VALUE = '100012343'
+order by spic.DSP_TITLE asc;
