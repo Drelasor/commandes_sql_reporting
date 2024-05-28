52e08da1599d583fc8108398426abf2a1f4a67d9
Raphaël Grand <otumbarg@gmail.com>
2024-05-28 18:10:23 +0200

    correction jointure dec

--- request.sql
+++ request.sql
@@ -28,7 +28,7 @@ left join RndSuite.RndtRqSp as rqspdec on rqspdec.RQ = decprop.RQ
 left join RndSuite.RndtSp as clientdec on clientdec.SP = rqspdec.SP and clientdec.SP_VERSION in ( select CASE rqspdec.SP_VERSION when -1 then (select TOP 1 first_value(x.SP_VERSION) over (order by x.ACTIVE desc, x.SP_VERSION desc) from RndSuite.RndtSp x where x.SP = clientdec.SP) else rqspdec.SP_VERSION end)
 left join RndSuite.RndtSpIc as decspic on decspic.SP = clientdec.SP 
 Left join RndSuite.RndtSpIcLang as decspiclang on decspiclang.SP = decspic.SP and decspiclang.SP_VERSION = decspic.SP_VERSION and decspiclang.IC = decspic.IC and decspiclang.ICNODE = decspic.ICNODE
-left join RndSuite.RndtSpIi as decspii on spii.SP = decspic.SP and decspii.SP_VERSION = decspic.SP_VERSION and decspii.ICNODE = decspic.ICNODE and decspii.IC = decspic.IC
+left join RndSuite.RndtSpIi as decspii on decspii.SP = decspic.SP and decspii.SP_VERSION = decspic.SP_VERSION and decspii.ICNODE = decspic.ICNODE and decspii.IC = decspic.IC
 Left join RndSuite.RndtSpIiLang as decspiilang on decspiilang.SP = decspii.SP and decspiilang.SP_VERSION = decspii.SP_VERSION and decspiilang.IC = decspii.IC and decspiilang.ICNODE = spii.ICNODE and decspiilang.IINODE = spii.IINODE
 
 
